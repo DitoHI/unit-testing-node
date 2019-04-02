9e3e1027bad077ec88200fcbb7f81f2e59634349
ditohafizh <ditohafizh@gmail.com>
2019-04-02 20:17:25 +0700

    video 3 - multiple context & subheading

--- test/test.js
+++ test/test.js
@@ -2,6 +2,22 @@ const assert = require('assert');
 
 describe('File to be tested', () => {
     context('function to be tested', () => {
+        // before(() => {
+        //     console.log('=====before');
+        // });
+        //
+        // after(() => {
+        //     console.log('=====after');
+        // });
+        //
+        // beforeEach(() => {
+        //     console.log('=====beforeEach');
+        // });
+        //
+        // afterEach(() => {
+        //     console.log('=====afterEach');
+        // });
+
         it('should do something', () => {
             assert.equal(1, 1);
         });
@@ -12,4 +28,8 @@ describe('File to be tested', () => {
 
         it('this is a pending test');
     });
+
+    context('another function', () => {
+        it('should do something');
+    })
 });
